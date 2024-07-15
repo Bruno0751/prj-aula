4498dae4aa36a50d07abe30b49faa0b3a0197fec
Bruno Gressler da Silveira <brunogressler1@gmail.com>
2024-07-15 01:04:49 -0300

    update

--- src/view/Main.java
+++ src/view/Main.java
@@ -1,12 +1,7 @@
 package view;
 
 import java.util.Scanner;
-import javax.xml.transform.Source;
 
-/**
- *
- * @author Bruno
- */
 public class Main {
 
     public static void main(String[] args) {
@@ -17,31 +12,46 @@ public class Main {
         String nome = "bruno";
         // VALORES INTEIROS
         double numero2;
-        double numero1;
-        double res;
+        double numero1 = 15;
+        double res = 0;
         // LOGICOS        
-        boolean logico;
+        boolean logico = true;
         double quebrados;
-
+        
         //**************************************ENTADA DE DADOS
-        System.out.print("Digite um numero com ,: ");
-        // LEIA
-        numero1 = scanner.nextDouble();
-        System.out.print("Digite um numero: ");
-        numero2 = scanner.nextDouble();
+//        System.out.print("Digite um numero com ,: ");
+//        // LEIA
+//        numero1 = scanner.nextDouble();
+//        System.out.print("Digite um numero: ");
+//        numero2 = scanner.nextDouble();
         //****************************************************
         
         //*************************************PROCESSAMENTO
         // OPERADORES ARITIMETICOS
-        // +-MAIS -MENOS / *-VEZES %-MOD
-        res = numero1 + numero2;
+        // +-MAIS -MENOS /_DIVIDIR *-VEZES %-MOD
+        
+        // OPERADORES LóGICOS
+        // !-NEGAÇÂO or-OU &&-E
+        
+        // OPERADORES COMPARATIVOS
+        
+//        if () {
+//            System.out.println("verdadeira");
+//        } else {
+//            System.out.println("false");
+//        }
+//        System.out.println("fim");
+        
+        
+        
+//        res = numero1 + numero2;
 //        ******************************************************
 
 //        *************************************************SAIDA DE DADOS
         // ESCREVA
 //        System.out.print("ola: zn");
 //          HARD CODE
-        System.out.println("A soma de " + numero1 + " mais " + numero2 + " é igual a " + res);
+//        System.out.println("A soma de " + numero1 + " mais " + numero2 + " é igual a " + res);
         //888888888888888888888888888888888888888888888888888888
     }
 }
