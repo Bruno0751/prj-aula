5a17b8cbc166c544247aa704b5740d8f9f19ad9c
Bruno Gressler da Silveira <brunogressler1@gmail.com>
2024-07-15 23:25:58 -0300

    update

--- src/view/Main.java
+++ src/view/Main.java
@@ -4,54 +4,100 @@ import java.util.Scanner;
 
 public class Main {
 
+    public void equals() {
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+//        ESCREVA
+        System.out.println("");
+//        SE SE NÃO
+        if (true) {
+        }
+//        ESCRECOLHA O CASO
+        switch (1) {
+            case 1:
+                break;
+        }
+        try {
+        } catch (Exception e) {}
+//        NOMES DE CLASSE COM LETRAS MAIUSCULAS --- MinhaClasse
+//                NOME DE FUNCOES COM ()        --- minhaFuncao()
+//NOME DE VARIAVEIS COM LETRA MINUSCULA         --- minhaVariavel
+// PARA SE CHAMAR UMA FUNCAO DE UMA CLASSE BASTA CHAMAR .
         //*************************************PREPARAÇÂO DA MEMÒRIA
+        // TIPOS DE DADOS /  String, char, int, byte, double boolean 
         // CARCTERE
-        String nome = "bruno";
-        // VALORES INTEIROS
+        char x = 'h';
+
+        String a = "";
+
+        // VALORES INTEIROS---------------------------------------------
+        int idadex;
+        // VALORES QUEBRADOS---------------------------------------------
         double numero2;
-        double numero1 = 15;
+        int numero1 = 15;
         double res = 0;
-        // LOGICOS        
+        // LOGICOS   ---------------------------------------------     
         boolean logico = true;
         double quebrados;
-        
+
         //**************************************ENTADA DE DADOS
-//        System.out.print("Digite um numero com ,: ");
-//        // LEIA
-//        numero1 = scanner.nextDouble();
-//        System.out.print("Digite um numero: ");
-//        numero2 = scanner.nextDouble();
+//-----------------------------------------------------------------------------
+//        // scanner
+//        try {
+//            System.out.print("Digite um numero: ");
+//            numero1 = scanner.nextInt();
+//            System.out.print("Digite um numero: ");
+//            numero2 = scanner.nextDouble();
+//        } catch (Exception err) {
+//            System.out.println("erro");
+//        } finally {
+//            System.out.println("passa os dois");
+//        }
+
         //****************************************************
-        
         //*************************************PROCESSAMENTO
         // OPERADORES ARITIMETICOS
-        // +-MAIS -MENOS /_DIVIDIR *-VEZES %-MOD
-        
+        // + MAIS - MENOS / * % MOD
         // OPERADORES LóGICOS
-        // !-NEGAÇÂO or-OU &&-E
-        
-        // OPERADORES COMPARATIVOS
-        
-//        if () {
-//            System.out.println("verdadeira");
+        // ! || &&
+//        OPERADORES COMPARATIVOS
+//-----------------------------------------------------------------------------
+//function () {
+//    }
+//        String nome = "Fernando";
+//        byte idade = 27;
+//        if ( nome.equals("ghyhghyu") ) {
+//            System.out.println("verdadeiro");
 //        } else {
 //            System.out.println("false");
 //        }
-//        System.out.println("fim");
-        
-        
-        
-//        res = numero1 + numero2;
+//-----------------------------------------------------------------------------
+//        int op = 5;
+//        switch (op) {
+//            case 0:
+//                break;
+//            case 1:
+//                break;
+//            case 2:
+//                break;
+//            case 3:
+//                break;
+//            case 4:
+//                System.out.println("4");
+//                break;
+//            default:
+//                
+//                break;
+//        }
 //        ******************************************************
-
 //        *************************************************SAIDA DE DADOS
         // ESCREVA
 //        System.out.print("ola: zn");
 //          HARD CODE
 //        System.out.println("A soma de " + numero1 + " mais " + numero2 + " é igual a " + res);
-        //888888888888888888888888888888888888888888888888888888
+        scanner.close();
     }
 }
